b987dbd46c7166d6473f6c466e3534af1a2c1183
Office_shenyang <office@qq.com>
2021-10-11 16:18:13 +0800

    Signed-off-by: Office_shenyang <office@qq.com>

--- src/index.js
+++ src/index.js
@@ -1,17 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import reactDom from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+reactDom.render(
+    <h1>123</h1>,
+    document.getElementById('root')
+)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reactDom.render 有两个参数，第一个是要渲染的内容，第二个是要插入到页面的标签
